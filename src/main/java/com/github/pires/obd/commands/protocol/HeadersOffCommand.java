17bd8ef48dbf46fd6341d4f5b6a09ba3199379d0
roque <gitlab@roque.pl>
2025-01-05 19:56:07 +0100

    fix

--- src/main/java/com/github/pires/obd/commands/protocol/HeadersOffCommand.java
+++ src/main/java/com/github/pires/obd/commands/protocol/HeadersOffCommand.java
@@ -3,7 +3,6 @@
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -22,7 +21,7 @@ public class HeadersOffCommand extends ObdProtocolCommand {
      * <p>Constructor for HeadersOffCommand.</p>
      */
     public HeadersOffCommand() {
-        super("ATH0");
+        super("AT H0");
     }
 
     /**
