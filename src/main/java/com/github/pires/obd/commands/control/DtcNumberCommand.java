e6e31423b845e4f865f5f6f54722ac4711e9ecfa
roque <gitlab@roque.pl>
2025-01-06 19:55:41 +0100

    Fix for Mil response

--- src/main/java/com/github/pires/obd/commands/control/DtcNumberCommand.java
+++ src/main/java/com/github/pires/obd/commands/control/DtcNumberCommand.java
@@ -3,7 +3,6 @@
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -61,7 +60,7 @@ public class DtcNumberCommand extends ObdCommand {
      */
     public String getFormattedResult() {
         final String res = milOn ? "MIL is ON" : "MIL is OFF";
-        return res + codeCount + " codes";
+        return res + " (" + codeCount + " codes)";
     }
 
     /** {@inheritDoc} */
